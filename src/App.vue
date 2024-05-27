346fbc8d115d044c4982e8ccc4d391a13c1315bb
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-27 21:33:20 +0200

    added bonus-1 imported axios in html and created in App.Vue that generated 10 cards with axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue'
+import AppMain from './components/AppMain.vue';
+import store from './data/store.js'
 export default {
   components: {
     AppHeader,
@@ -9,8 +10,18 @@ export default {
 
   data() {
     return {
-
+      //richiamo lo store.js in return per far si che ho lo lista delle cards visibile qui
+      store
     }
+  },
+  created() {
+    //creo funzione che mi genera le card tramite un server esterno che poi inietto nella lista delle cards
+
+    axios.get(/*dopo num= decido quante carte creare e dopo offset= decido quante me ne deve saltare di quelle inziali della lista*/"https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0").then(risultato => {
+
+      //dichiaro che in store.js la lista cards è uguale al risultato che mi da la funzione che genera le card
+      this.store.cards = risultato.data.data
+    })
   }
 }
 </script>
