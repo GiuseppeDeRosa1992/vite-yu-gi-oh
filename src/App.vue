a2726dde31eea314f9070ecf5bcdf0c64bffef88
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-28 14:37:37 +0200

    imported axios with vue and removed script axios inside html

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
-import store from './data/store.js'
+import store from './data/store.js';
+import axios from 'axios';
 export default {
   components: {
     AppHeader,
